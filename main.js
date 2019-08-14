4b8cb3d3b47fa1b6e964c77ca04732ef29201708
Amdrija <andrija.jelenkovic@protonmail.com>
2019-08-14 12:51:17 +0200

    Add keyboard support

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ const equalsButton = document.querySelector("#equals");
 const dotButton = document.querySelector("#dot");
 
 let dotDisabled = false;
+let operationDisabled = true;
 
 const textbox = document.querySelector("#textbox");
 const statusbox = document.querySelector("#status");
+const maxNumberLength = 12;
 
 const operations = {
     "+": add,
@@ -17,75 +19,159 @@ const operations = {
     "/": divide,
 };
 
+document.querySelectorAll(".button").forEach((button) => button.addEventListener("click", (event) => {
+    event.target.classList.add("clicked");
+}));
+document.querySelectorAll(".button").forEach((button) => button.addEventListener("transitionend", (event) => {
+    event.target.classList.remove("clicked");
+}));
+
 numberButtons.forEach((button) => {
-    button.addEventListener("click", function (event) {
-        if (textbox.textContent.length < 12) {
-            textbox.textContent += event.target.textContent;
-        } else {
-            statusbox.textContent = "Expression too long."
-        }
+    button.addEventListener("click", (event) => {
+        operationDisabled = false;
+        addText(event.target.textContent)
     });
 });
 
 operationButtons.forEach((button) => {
     button.addEventListener("click", function (event) {
-        if (textbox.textContent.length < 12) {
-            textbox.textContent += event.target.textContent;
-        } else {
-            statusbox.textContent = "Expression too long."
+        if (!operationDisabled) {
+            addText(event.target.textContent);
+            dotDisabled = false;
+            operationDisabled = true;
         }
-        dotDisabled = false;
     });
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", clearText);
+
+backspaceButton.addEventListener("click", backspaceText);
+
+dotButton.addEventListener("click", dotPress);
+
+equalsButton.addEventListener("click", calculateResultText);
+
+window.addEventListener("keydown", keyPress);
+
+function keyPress(event) {
+    if (event.shiftKey) {
+        switch (event.keyCode) {
+            case 56:
+                document.querySelector("#multiplication").click();
+                break;
+            case 61:
+                document.querySelector("#addition").click();
+                break;
+            case 8:
+                document.querySelector("#clear").click();
+                break;
+        }
+        return;
+    }
+
+    if (event.keyCode >= 48 && event.keyCode <= 57) {
+        document.querySelector(`#number-${event.keyCode - 48}`).click();
+        return;
+    };
+
+    switch (event.keyCode) {
+        case 191:
+            document.querySelector("#division").click();
+            break;
+        case 173:
+            document.querySelector("#subtraction").click();
+            break;
+        case 190:
+            document.querySelector("#dot").click();
+            break;
+        case 61:
+        case 13:
+            document.querySelector("#equals").click();
+            break;
+        case 8:
+            document.querySelector("#backspace").click();
+            break;
+    }
+
+}
+
+
+
+function dotPress() {
+    if (!dotDisabled) {
+        dotDisabled = true;
+        addText(".");
+    }
+}
+
+function addText(text) {
+    if (textbox.textContent.length < maxNumberLength) {
+        textbox.textContent += text;
+    } else {
+        statusbox.textContent = "Expression too long."
+    }
+}
+
+function clearText() {
     textbox.textContent = "";
     statusbox.textContent = "";
     dotDisabled = false;
-});
+    operationDisabled = true;
+}
 
-backspaceButton.addEventListener("click", () => {
+function backspaceText() {
     if (textbox.textContent.charAt(textbox.textContent.length - 1) === ".") {
         dotDisabled = false;
     }
+    if (textbox.textContent.charAt(textbox.textContent.length - 1) in operations) {
+        operationDisabled = false;
+    }
     textbox.textContent = textbox.textContent.slice(0, -1);
-    if (textbox.textContent.length < 12) {
+    if (textbox.textContent.length < maxNumberLength) {
         statusbox.textContent = "";
     }
-});
+}
 
-dotButton.addEventListener("click", (event) => {
-    if (!dotDisabled) {
-        dotDisabled = true;
-        if (textbox.textContent.length < 12) {
-            textbox.textContent += event.target.textContent;
-        } else {
-            statusbox.textContent = "Expression too long."
-        }
-    }
-});
-
-equalsButton.addEventListener("click", () => {
-
+function calculateResultText() {
     if (!isNumber(textbox.textContent.slice(-1)) && textbox.textContent.slice(-1) != ".") {
         textbox.textContent = textbox.textContent.slice(0, -1);
     }
 
     let result = +evaluate(textbox.textContent);
-    console.log(result);
     if (isNaN(result)) {
         textbox.textContent = "Error";
         statusbox.textContent = "Division by zero.";
         return;
     }
 
-    textbox.textContent = Math.floor(result * 100000000000) / 100000000000;
-    if (textbox.textContent.length > 13) {
-        textbox.textContent = (+textbox.textContent).toExponential(5);
+    textbox.textContent = variableRound(result);
+    statusbox.textContent = "";
+
+    /*
+    ! DON'T ENABLE THE DOT AGAIN, IF THE RESULT IS DECIMAL, YOU DON'T WANT TO ADD ANOTHER DOT
+     */
+    operationDisabled = false;
+}
+
+function variableRound(number) {
+    number = number.toString();
+    let dotPosition = number.indexOf(".");
+
+    if (dotPosition == -1) {
+        if (number.length > maxNumberLength) {
+            return (+number).toExponential(5);
+        }
+        return number;
     }
 
-    statusbox.textContent = "";
-})
+    if (dotPosition > maxNumberLength) {
+        return (+number).toExponential(5);
+    }
+
+    let power10 = Math.pow(10, maxNumberLength - dotPosition);
+    return Math.round(+number * power10) / power10;
+
+}
 
 function add(a, b) {
     return a + b;
